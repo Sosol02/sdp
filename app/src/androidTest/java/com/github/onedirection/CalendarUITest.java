688afc1baeccc4b86be27ac6a21f88b498ea8d81
justagoodguy-omega <61226987+justagoodguy-omega@users.noreply.github.com>
2021-03-11 22:24:17 +0100

    implements the calendar UI (#22)
    * implements the calendar UI
    
    * adds tests to the calendar
    
    * Modify test class CalendarUITest
    
    * Delete MainActivity.java
    
    * Delete activity_main.xml
    
    Co-authored-by: Paul Oliver <paul.oliver@epfl.ch>
    Co-authored-by: Noe De Santo <noe.desanto@epfl.ch>

---
+++ app/src/androidTest/java/com/github/onedirection/CalendarUITest.java
@@ -0,0 +1,52 @@
+package com.github.onedirection;
+
+import android.view.View;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+
+public class CalendarUITest {
+
+
+        @Rule
+        public ActivityScenarioRule<CalendarUI> mCalendarActivity =
+                new ActivityScenarioRule<>(CalendarUI.class);
+
+        @Test
+        public void calendarShouldAppear(){
+            onView(withIndex(withId(R.id.calendarView), 0)).check(matches(isDisplayed()));
+        }
+
+        public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+            return new TypeSafeMatcher<View>() {
+                int currentIndex = 0;
+
+                @Override
+                public void describeTo(Description description) {
+                    description.appendText("with index: ");
+                    description.appendValue(index);
+                    matcher.describeTo(description);
+                }
+
+                @Override
+                public boolean matchesSafely(View view) {
+                    return matcher.matches(view) && currentIndex++ == index;
+                }
+            };
+        }
+}
