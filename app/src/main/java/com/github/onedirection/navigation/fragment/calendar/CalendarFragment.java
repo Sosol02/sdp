fc28a1dce9d98b4c11ef3b5f571af034d6bd37ff
Paul Oliver <paul.oliver@epfl.ch>
2021-03-14 10:30:05 +0100

    change de calendar used because previous version could not support events correctly

--- app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CalendarFragment.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CalendarFragment.java
@@ -1,8 +1,8 @@
 package com.github.onedirection.navigation.fragment.calendar;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,18 +12,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.CalendarView;
+
 
 import com.github.onedirection.R;
 
 public class CalendarFragment extends Fragment {
 
     private CalendarViewModel mViewModel;
+    private CalendarView calendarView;
+    private String startDate, endDate;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+        //calendarView = (CalendarView) root.findViewById(R.id.calendarView);
         return root;
     }
 
@@ -32,5 +36,8 @@ public class CalendarFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(CalendarViewModel.class);
     }
+
+
+
 
 }
