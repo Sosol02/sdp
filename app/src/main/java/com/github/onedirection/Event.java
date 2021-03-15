e292cb207855fe13f1e8d30c23a4dde39860c6bd
hugo <hugo.lanfranchi@gmail.com>
2021-03-15 17:51:45 +0100

    Improved code

--- app/src/main/java/com/github/onedirection/Event.java
+++ app/src/main/java/com/github/onedirection/Event.java
@@ -11,71 +11,61 @@ class Event {
     final private String start_time;
     final private String end_time;
 
-
     public Event(int id,String name, String location, String date, String start_time, String end_time){
-
         this.name = Objects.requireNonNull(name);
         this.location = Objects.requireNonNull(location);
         this.date = Objects.requireNonNull(date);
         this.start_time = Objects.requireNonNull(start_time);
         this.end_time = Objects.requireNonNull(end_time);
         this.id = id;
-
     }
 
-    public Event set_name(String new_value){
-
+    public Event setName(String new_value){
 
         return Objects.requireNonNull(new_value) == this.name?this : new Event(id,new_value,location,date,start_time,end_time);
     }
 
-    public Event set_location(String new_value){
-
+    public Event setLocation(String new_value){
 
         return Objects.requireNonNull(new_value) == this.location? this: new Event(id,name,new_value,date,start_time,end_time);
     }
 
-    public Event set_date(String new_value){
+    public Event setDate(String new_value){
 
         return Objects.requireNonNull(new_value) == this.date? this:new Event(id,name,location,new_value,start_time,end_time);
     }
 
-    public Event set_start_time(String new_value){
+    public Event setStartTime(String new_value){
 
         return Objects.requireNonNull(new_value) == this.start_time? this: new Event(id,name,location,date,new_value,end_time);
     }
 
-    public Event set_end_time(String new_value){
+    public Event setEndTime(String new_value){
 
         return Objects.requireNonNull(new_value) == this.end_time? this:new Event(id,name,location,date,start_time,new_value);
     }
 
-    public int get_id(){
+    public int getId(){
         return id;
     }
 
-    public String get_date(){
+    public String getDate(){
         return date;
     }
 
-    public String get_location(){
+    public String getLocation(){
         return location;
     }
 
-    public String get_name(){
+    public String getName(){
         return name;
     }
 
-    public String get_start_time(){
+    public String getStartTime(){
         return start_time;
     }
 
-    public String get_end_time(){
+    public String getEndTime(){
         return end_time;
-    }
-
-    public String serialize(){
-
-        return "";
     }
 }
