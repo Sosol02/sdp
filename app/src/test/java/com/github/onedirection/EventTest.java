0f1e1b252f6c56920dfe6829baa9cd0252f73a62
Noe De Santo <noe.desanto@epfl.ch>
2021-03-16 21:41:42 +0100

    Adapt tests to new interface

--- app/src/test/java/com/github/onedirection/EventTest.java
+++ app/src/test/java/com/github/onedirection/EventTest.java
@@ -1,107 +1,76 @@
 package com.github.onedirection;
 
+import com.github.onedirection.geocoding.NamedCoordinates;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.time.ZonedDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 
 public class EventTest {
+
+    private final static int ID = 001;
+    private final static String NAME = "Event name";
+    private final static NamedCoordinates LOCATION = new NamedCoordinates(0, 0, "Location name");
+    private final static ZonedDateTime START_TIME = ZonedDateTime.now();
+    private final static ZonedDateTime END_TIME = ZonedDateTime.now().plusHours(1);
+
+    private final static Event EVENT = new Event(ID, NAME, LOCATION, START_TIME, END_TIME);
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testEventWithNullArgument() {
+        int id = 001;
+        String name = "name";
+        NamedCoordinates location = new NamedCoordinates(0, 0, "name");
+        ZonedDateTime start_time = ZonedDateTime.now();
+        ZonedDateTime end_time = ZonedDateTime.now().plusHours(1);
+
+        assertThrows(NullPointerException.class, () -> new Event(id, null, location, start_time, end_time));
+        assertThrows(NullPointerException.class, () -> new Event(id, name, null, start_time, end_time));
+        assertThrows(NullPointerException.class, () -> new Event(id, name, location, null, end_time));
+        assertThrows(NullPointerException.class, () -> new Event(id, name, location, start_time, null));
     }
 
     @Test
-    public void testEventWithNullArgument(){
-        int id = 001;
-        String name = "name";
-        String location = "location";
-        String date = "date";
-        String start_time = "start_time";
-        String end_time = "end_time";
+    public void testEventSetNameAndGet() {
+        final String newName = "changed_parameter";
 
-        assertThrows(NullPointerException.class,() -> new Event(id,null,location,date,start_time,end_time));
-        assertThrows(NullPointerException.class,() -> new Event(id,name,null,date,start_time,end_time));
-        assertThrows(NullPointerException.class,() -> new Event(id,name,location,null,start_time,end_time));
-        assertThrows(NullPointerException.class,() -> new Event(id,name,location,date,null,end_time));
-        assertThrows(NullPointerException.class,() -> new Event(id,name,location,date,start_time,null));
+        assertThrows(NullPointerException.class, () -> EVENT.setName(null));
+        Event eventChanged = EVENT.setName(newName);
+        assertEquals(newName, eventChanged.getName());
+        assertEquals(NAME, EVENT.getName());
     }
 
     @Test
-    public void testEventSetNameAndGet(){
-        int id = 001;
-        String name = "name";
-        String location = "location";
-        String date = "date";
-        String start_time = "start_time";
-        String end_time = "end_time";
-        Event event = new Event(id,name,location,date,start_time,end_time);
-        assertThrows(NullPointerException.class, () -> event.setName(null));
-        Event eventChanged = event.setName("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getName());
-        eventChanged.setName("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getName());
+    public void testEventSetLocationAndGet() {
+        final NamedCoordinates newLoc = new NamedCoordinates(1, 1, "New location name");
+
+        assertThrows(NullPointerException.class, () -> EVENT.setLocation(null));
+        Event eventChanged = EVENT.setLocation(newLoc);
+        assertEquals(newLoc, eventChanged.getLocation());
+        assertEquals(LOCATION, EVENT.getLocation());
     }
 
     @Test
-    public void testEventSetLocationAndGet(){
-        int id = 001;
-        String name = "name";
-        String location = "location";
-        String date = "date";
-        String start_time = "start_time";
-        String end_time = "end_time";
-        Event event = new Event(id,name,location,date,start_time,end_time);
-        assertThrows(NullPointerException.class, () -> event.setLocation(null));
-        Event eventChanged = event.setLocation("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getLocation());
-        eventChanged.setLocation("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getLocation());
+    public void testEventSetStartTimeAndGet() {
+        final ZonedDateTime newTime = ZonedDateTime.now().minusHours(1);
+
+        assertThrows(NullPointerException.class, () -> EVENT.setStartTime(null));
+        Event eventChanged = EVENT.setStartTime(newTime);
+        assertEquals(newTime, eventChanged.getStartTime());
+        assertEquals(START_TIME, EVENT.getStartTime());
     }
 
     @Test
-    public void testEventSetDateAndGet(){
-        int id = 001;
-        String name = "name";
-        String location = "location";
-        String date = "date";
-        String start_time = "start_time";
-        String end_time = "end_time";
-        Event event = new Event(id,name,location,date,start_time,end_time);
-        assertThrows(NullPointerException.class, () -> event.setDate(null));
-        Event eventChanged = event.setDate("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getDate());
-        eventChanged.setDate("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getDate());
-    }
+    public void testEventSetEndTimeAndGet() {
+        final ZonedDateTime newTime = ZonedDateTime.now().plusYears(1);
 
-    @Test
-    public void testEventSetStartTimeAndGet(){
-        int id = 001;
-        String name = "name";
-        String location = "location";
-        String date = "date";
-        String start_time = "start_time";
-        String end_time = "end_time";
-        Event event = new Event(id,name,location,date,start_time,end_time);
-        assertThrows(NullPointerException.class, () -> event.setStartTime(null));
-        Event eventChanged = event.setStartTime("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getStartTime());
-        eventChanged.setStartTime("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getStartTime());
-    }
-
-    @Test
-    public void testEventSetEndTimeAndGet(){
-        int id = 001;
-        String name = "name";
-        String location = "location";
-        String date = "date";
-        String start_time = "start_time";
-        String end_time = "end_time";
-        Event event = new Event(id,name,location,date,start_time,end_time);
-        assertThrows(NullPointerException.class, () -> event.setEndTime(null));
-        Event eventChanged = event.setEndTime("changed_parameter");
-        assertEquals("changed_parameter",eventChanged.getEndTime());
+        assertThrows(NullPointerException.class, () -> EVENT.setEndTime(null));
+        Event eventChanged = EVENT.setEndTime(newTime);
+        assertEquals(newTime, eventChanged.getEndTime());
+        assertEquals(START_TIME, EVENT.getStartTime());
     }
 }
