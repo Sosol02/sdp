d006bc6e33a14c959191e75197f628fbf1fb6dcb
Nicolas Wagner <nicolas.wagner@epfl.ch>
2021-03-18 02:33:00 +0100

    updated test for sign fragment

--- app/src/androidTest/java/com/github/onedirection/SignFragmentTest.java
+++ app/src/androidTest/java/com/github/onedirection/SignFragmentTest.java
@@ -1,7 +1,9 @@
 package com.github.onedirection;
 
+import android.content.Context;
 import android.widget.TextView;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -15,30 +17,38 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
-public class LoginFragmentTest {
+public class SignFragmentTest {
 
     @Rule
     public ActivityScenarioRule<NavigationActivity> testRule = new ActivityScenarioRule<>(NavigationActivity.class);
 
+    private static final Context ctx = ApplicationProvider.getApplicationContext();
+
     @Test
-    public void testLogin() {
+    public void testNormalSignInAndLogout() {
+        onView(withId(R.id.nav_header_email)).check(matches(withText(equalTo(ctx.getString(R.string.nav_header_email)))));
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_sign)).perform(ViewActions.click());
 
         onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
                 .check(matches(withText(R.string.menu_sign)));
 
-        onView(withId(R.id.email)).perform(ViewActions.clearText(), ViewActions.typeText("Test"));
-
-        onView(withId(R.id.email)).perform(ViewActions.typeText("@test.test"));
-        onView(withId(R.id.password)).perform(ViewActions.typeText("345656"));
+        onView(withId(R.id.email)).perform(ViewActions.clearText(), ViewActions.typeText(ctx.getString(R.string.test_account)));
+        onView(withId(R.id.password)).perform(ViewActions.typeText(ctx.getString(R.string.test_password)));
         onView(withId(R.id.sign)).perform(ViewActions.click());
+        //onView(withId(R.id.nav_header_email)).check(matches(withText(equalTo(ctx.getString(R.string.test_account)))));
+
+        /*onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_logout)).perform(ViewActions.click());*/
+        //onView(withId(R.id.nav_header_email)).check(matches(withText(equalTo(ctx.getString(R.string.nav_header_email)))));
     }
 }
