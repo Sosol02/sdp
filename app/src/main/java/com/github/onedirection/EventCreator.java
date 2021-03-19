93ec5c6d0f6ee1063eafee5acad6103afb9d3e54
Noe De Santo <noe.desanto@epfl.ch>
2021-03-19 11:50:05 +0100

    Update event creator to allow date/time picking

--- app/src/main/java/com/github/onedirection/EventCreator.java
+++ app/src/main/java/com/github/onedirection/EventCreator.java
@@ -1,12 +1,23 @@
 package com.github.onedirection;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.util.Locale;
 
 public class EventCreator extends AppCompatActivity {
     public static final String EXTRA_NAME = "NAME";
@@ -15,10 +26,19 @@ public class EventCreator extends AppCompatActivity {
     public static final String EXTRA_START_TIME = "START_TIME";
     public static final String EXTRA_END_TIME = "END_TIME";
 
+    public static final String EXTRA_EVENT_NAME = "EVENT";
+    public static final Class<Event> EXTRA_EVENT_TYPE = Event.class;
+
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_creator);
+
+        startTime = ZonedDateTime.now();
+        endTime = startTime.plusHours(1);
 
         findViewById(R.id.buttonEventAdd).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,26 +46,78 @@ public class EventCreator extends AppCompatActivity {
                 validateEvent(v);
             }
         });
+
+        updateTimeDates();
     }
 
     public void validateEvent(View view) {
-        Intent intent = new Intent(this,EventsView.class);
-        EditText name = (EditText) findViewById(R.id.editTextName);
-        EditText location = (EditText) findViewById(R.id.editTextLocation);
-        EditText date = (EditText) findViewById(R.id.editTextDate);
-        EditText start_time = (EditText) findViewById(R.id.editTextEndTime);
-        EditText end_time = (EditText) findViewById(R.id.editTextStartTime);
+        Intent intent = new Intent(this, EventsView.class);
+        EditText name = findViewById(R.id.editEventName);
+        EditText location = findViewById(R.id.editEventLocation);
+        //EditText start_time = findViewById(R.id.editStartDate);
+        //DatePicker end_time = (DatePicker) findViewById(R.id.editEndDate);
         //generate id?
         //Event event = new Event(id,name.getText().toString(),location.getText().toString(),date.getText().toString(),start_time.getText().toString(),end_time.getText().toString());
         //send event to the db
-        intent.putExtra(EXTRA_NAME,name.getText().toString());
-        intent.putExtra(EXTRA_LOCATION,location.getText().toString());
-        intent.putExtra(EXTRA_DATE,date.getText().toString());
-        intent.putExtra(EXTRA_START_TIME,start_time.getText().toString());
-        intent.putExtra(EXTRA_END_TIME,end_time.getText().toString());
+        intent.putExtra(EXTRA_NAME, name.getText().toString());
+        intent.putExtra(EXTRA_LOCATION, location.getText().toString());
+        //intent.putExtra(EXTRA_START_TIME, start_time.getDay());
+        //intent.putExtra(EXTRA_END_TIME, end_time.getText().toString());
 
         startActivity(intent);
     }
 
+    private void updateTimeDates(){
+        Button startTimeBtn = findViewById(R.id.buttonStartTime);
+        Button endTimeBtn = findViewById(R.id.buttonEndTime);
+        Button startDateBtn = findViewById(R.id.buttonStartDate);
+        Button endDateBtn = findViewById(R.id.buttonEndDate);
 
+        startTimeBtn.setText(LocalTime.of(startTime.getHour(), startTime.getMinute()).toString());
+        endTimeBtn.setText(LocalTime.of(endTime.getHour(), endTime.getMinute()).toString());
+        startDateBtn.setText(startTime.toLocalDate().toString());
+        endDateBtn.setText(endTime.toLocalDate().toString());
+    }
+
+    public void showStartTimePicker(View v){
+        TimePickerDialog timePicker = new TimePickerDialog(v.getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                startTime = ZonedDateTime.of(startTime.toLocalDate(), LocalTime.of(hourOfDay, minute), startTime.getZone());
+                updateTimeDates();
+            }
+        }, startTime.getHour(), startTime.getMinute(), true);
+        timePicker.show();
+    }
+
+    public void showEndTimePicker(View v){
+        TimePickerDialog timePicker = new TimePickerDialog(v.getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                endTime = ZonedDateTime.of(endTime.toLocalDate(), LocalTime.of(hourOfDay, minute), endTime.getZone());
+                updateTimeDates();
+            }
+        }, endTime.getHour(), endTime.getMinute(), true);
+        timePicker.show();
+    }
+
+    public void showStartDatePicker(View v){
+        DatePickerDialog datePicker = new DatePickerDialog(v.getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                startTime = ZonedDateTime.of(LocalDate.of(year, month, dayOfMonth), startTime.toLocalTime(), startTime.getZone());
+            }
+        }, startTime.getYear(), startTime.getMonthValue(), startTime.getDayOfMonth());
+        datePicker.show();
+    }
+
+    public void showEndDatePicker(View v){
+        DatePickerDialog datePicker = new DatePickerDialog(v.getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                startTime = ZonedDateTime.of(LocalDate.of(year, month, dayOfMonth), startTime.toLocalTime(), startTime.getZone());
+            }
+        }, startTime.getYear(), startTime.getMonthValue(), startTime.getDayOfMonth());
+        datePicker.show();
+    }
 }
