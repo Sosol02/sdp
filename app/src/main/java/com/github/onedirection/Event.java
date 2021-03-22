c370f9cdba19c40eff0b9b97e3656d0468c96aa4
justagoodguy-omega <61226987+justagoodguy-omega@users.noreply.github.com>
2021-03-22 10:19:20 +0100

    Change "geocoding" imports to "geolocalization"

--- app/src/main/java/com/github/onedirection/Event.java
+++ app/src/main/java/com/github/onedirection/Event.java
@@ -1,7 +1,7 @@
 package com.github.onedirection;
 
-import com.github.onedirection.geocoding.Coordinates;
-import com.github.onedirection.geocoding.NamedCoordinates;
+import com.github.onedirection.geolocalization.Coordinates;
+import com.github.onedirection.geolocalization.NamedCoordinates;
 import com.github.onedirection.utils.Id;
 
 import java.io.Serializable;
