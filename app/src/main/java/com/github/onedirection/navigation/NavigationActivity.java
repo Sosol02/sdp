ab6df25a2dd1aebeca13652dabbbd03f79210816
Nicolas Wagner <nicolas.wagner@epfl.ch>
2021-03-24 17:54:47 +0100

    added some actions possible on google maps

--- app/src/main/java/com/github/onedirection/navigation/NavigationActivity.java
+++ app/src/main/java/com/github/onedirection/navigation/NavigationActivity.java
@@ -2,6 +2,8 @@ package com.github.onedirection.navigation;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
--- app/src/main/java/com/github/onedirection/navigation/fragment/map/MapFragment.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/map/MapFragment.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -20,19 +21,19 @@ import com.github.onedirection.R;
 
 public class MapFragment extends Fragment {
 
+    private UiSettings mUiSettings;
+
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+
+            mUiSettings = googleMap.getUiSettings();
+            mUiSettings.setZoomControlsEnabled(true);
+            mUiSettings.setMyLocationButtonEnabled(true);
+            mUiSettings.setMapToolbarEnabled(false);
+
+
             LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
