d9ce30749b7e16ee057d538972ff4dc6608719f6
Noe De Santo <noe.desanto@epfl.ch>
2021-03-28 11:26:57 +0200

    Add tests for reverse geocoding and fix nominatim geocoding

--- app/src/main/java/com/github/onedirection/geocoding/NominatimGeocoding.java
+++ app/src/main/java/com/github/onedirection/geocoding/NominatimGeocoding.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.onedirection.utils.HTTP;
 import com.github.onedirection.utils.Monads;
@@ -15,11 +16,12 @@ import org.json.JSONObject;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 /** OpenStreetMap's geocoding service. */
 public final class NominatimGeocoding implements GeocodingService {
     private static final String SEARCH_QUERY_FORMAT = "https://nominatim.openstreetmap.org/search?format=json&q=%s";
-    private static final String REVERSE_QUERY_FORMAT = "https://nominatim.openstreetmap.org/reverse?format=json&lat=%d&lon=%d";
+    private static final String REVERSE_QUERY_FORMAT = "https://nominatim.openstreetmap.org/reverse?format=json&lat=%s&lon=%s";
 
     private static final String NAME_FIELD = "display_name";
     private static final String LAT_FIELD = "lat";
@@ -42,14 +44,13 @@ public final class NominatimGeocoding implements GeocodingService {
     }
 
     private static String generateReverseRequestURL(Coordinates coordinates){
-        return String.format(SEARCH_QUERY_FORMAT, coordinates.latitude, coordinates.longitude);
+        return String.format(REVERSE_QUERY_FORMAT, coordinates.latitude, coordinates.longitude);
     }
 
-    private static Optional<NamedCoordinates> parseResult(JSONArray json){
-        Log.d(LOGCAT_TAG, "Parsing: " + json);
+    private static Optional<NamedCoordinates> parseResult(JSONObject json){
+        Log.d(LOGCAT_TAG, "Parsing location: " + json);
         try {
-            JSONObject best = json.getJSONObject(0);
-            return Optional.of(new NamedCoordinates(best.getDouble(LAT_FIELD), best.getDouble(LON_FIELD), best.getString(NAME_FIELD)));
+            return Optional.of(new NamedCoordinates(json.getDouble(LAT_FIELD), json.getDouble(LON_FIELD), json.getString(NAME_FIELD)));
         }
         catch(Exception e){
             Log.d(LOGCAT_TAG, "Parsing failed");
@@ -57,7 +58,17 @@ public final class NominatimGeocoding implements GeocodingService {
         }
     }
 
-    private CompletableFuture<JSONArray> sendRequest(String url){
+    private static Optional<NamedCoordinates> parseResult(JSONArray json, int idx){
+        try {
+            return parseResult(json.getJSONObject(idx));
+        }
+        catch(Exception e){
+            Log.d(LOGCAT_TAG, "Parsing failed");
+            return Optional.empty();
+        }
+    }
+
+    private CompletableFuture<JSONArray> sendArrayRequest(String url){
         CompletableFuture<JSONArray> result = new CompletableFuture<>();
 
         Log.d(LOGCAT_TAG, "Request: " + url);
@@ -72,19 +83,32 @@ public final class NominatimGeocoding implements GeocodingService {
         return result;
     }
 
-    private CompletableFuture<NamedCoordinates> requestToResult(String request){
-        return Monads.flatten(
-                sendRequest(request).thenApply(NominatimGeocoding::parseResult)
+    private CompletableFuture<JSONObject> sendObjectRequest(String url){
+        CompletableFuture<JSONObject> result = new CompletableFuture<>();
+
+        Log.d(LOGCAT_TAG, "Request: " + url);
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                result::complete,
+                result::completeExceptionally
         );
+        requestQueue.add(request);
+        return result;
     }
 
     @Override
     public CompletableFuture<NamedCoordinates> getBestNamedCoordinates(String locationName) {
-        return requestToResult(generateSearchRequestURL(locationName));
+        return Monads.flatten(
+                sendArrayRequest(generateSearchRequestURL(locationName)).thenApply(r -> parseResult(r, 0))
+        );
     }
 
     @Override
     public CompletableFuture<NamedCoordinates> getBestNamedCoordinates(Coordinates coordinates) {
-        return requestToResult(generateReverseRequestURL(coordinates));
+        return Monads.flatten(
+                sendObjectRequest(generateReverseRequestURL(coordinates)).thenApply(NominatimGeocoding::parseResult)
+        );
     }
 }
