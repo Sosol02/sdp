e87ed726c4963bf404ba8aeeb0ab10636ceb93ac
Nicolas Wagner <nicolas.wagner@epfl.ch>
2021-03-30 16:47:47 +0200

    Add my button location into the mapView

--- app/src/main/java/com/github/onedirection/navigation/fragment/map/MapFragment.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/map/MapFragment.java
@@ -11,8 +11,12 @@ import androidx.fragment.app.Fragment;
 
 import com.github.onedirection.R;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
 public class MapFragment extends Fragment {
@@ -30,9 +34,18 @@ public class MapFragment extends Fragment {
 
         mapView = (MapView) view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
-
-        }));
+        mapView.getMapAsync(mapboxMap -> {
+            mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {});
+            view.findViewById(R.id.my_location_button).setOnClickListener(view1 -> {
+                //TODO add location service
+                CameraPosition position = new CameraPosition.Builder()
+                        .target(new LatLng(51.50550, -0.07520))
+                        .zoom(15)
+                        .tilt(20)
+                        .build();
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 1000);
+            });
+        });
         return view;
     }
 
