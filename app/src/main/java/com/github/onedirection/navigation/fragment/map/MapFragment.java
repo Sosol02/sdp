c8063a3034f7b0564a4762351f1a6698a5eb4e9e
solene <solene.husseini@gmail.com>
2021-03-31 18:02:07 +0200

    Implement bottom sheet for events

--- app/src/main/java/com/github/onedirection/navigation/fragment/map/MapFragment.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/map/MapFragment.java
@@ -4,20 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.onedirection.Event;
 import com.github.onedirection.R;
+import com.github.onedirection.utils.Id;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class MapFragment extends Fragment {
 
     private MapView mapView;
+    private BottomSheetBehavior<View> bottomSheetBehavior;
+    private TextView event_name;
+    private TextView event_time_start;
+    private TextView event_time_end;
+    private TextView event_location;
 
     @Nullable
     @Override
@@ -33,12 +47,43 @@ public class MapFragment extends Fragment {
         mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
 
         }));
+
+        View bottomSheet = view.findViewById(R.id.fragment_map_bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        Button button = view.findViewById(R.id.fragment_map_test_show_bottom_sheet);
+        button.setOnClickListener(x -> showBottomSheet(view));
+
+        event_name = view.findViewById(R.id.fragment_map_event_name);
+        event_time_start = view.findViewById(R.id.fragment_map_event_time_start);
+        event_time_end = view.findViewById(R.id.fragment_map_event_time_end);
+        event_location = view.findViewById(R.id.fragment_map_event_location);
+
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
 
+    public void showBottomSheet(View view) {
+        /*
+        Event event = new Event(Id.generateRandom(), "Test event", "Mooon",
+                ZonedDateTime.of(2021, 4, 2, 13, 42, 56, 0, ZoneId.systemDefault()),
+                ZonedDateTime.of(2021, 4, 2, 13, 58, 56, 0, ZoneId.systemDefault()));
+        setBottomSheetEvent(event); // */
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+
+    public void setBottomSheetEvent(Event event) {
+        event_name.setText(event.getName());
+        ZonedDateTime start = event.getStartTime();
+        event_time_start.setText(String.format("%s %s %dh%d", start.getMonth().getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault()), start.getDayOfMonth(), start.getHour(), start.getMinute()));
+        ZonedDateTime end = event.getEndTime();
+        event_time_end.setText(String.format("%s %s %dh%d", end.getMonth().getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault()), end.getDayOfMonth(), end.getHour(), end.getMinute()));
+        event_location.setText(event.getLocationName());
     }
 }
