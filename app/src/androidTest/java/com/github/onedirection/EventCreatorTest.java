ffc99213aa9433c429c2e0ff2e6832b3ba15ab4a
Noe De Santo <noe.desanto@epfl.ch>
2021-04-03 11:17:11 +0200

    Improve test coverage on EventCreator

--- app/src/androidTest/java/com/github/onedirection/EventCreatorTest.java
+++ app/src/androidTest/java/com/github/onedirection/EventCreatorTest.java
@@ -30,22 +30,32 @@ import org.junit.runner.RunWith;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.stringContainsInOrder;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -54,10 +64,14 @@ public class EventCreatorTest {
     private final static Id ID = Id.generateRandom();
     private final static String NAME = "Event name";
     private final static String LOCATION_NAME = "Location name";
+    private final static NamedCoordinates LOCATION = new NamedCoordinates(0, 0, LOCATION_NAME);
     private final static ZonedDateTime START_TIME = ZonedDateTime.now().plusDays(1);
     private final static ZonedDateTime END_TIME = ZonedDateTime.now().plusDays(2);
 
-    private final static Event EVENT = new Event(ID, NAME, LOCATION_NAME, START_TIME, END_TIME);
+    private final static String EPFL_QUERY = "EPFL";
+    private final static String EPFL_CANTON = "Vaud";
+
+    private final static Event EVENT = new Event(ID, NAME, LOCATION, START_TIME, END_TIME);
     private final static Event EVENT_BIS = new Event(
             ID,
             "Other name",
@@ -88,16 +102,7 @@ public class EventCreatorTest {
         IdlingRegistry.getInstance().unregister(idling);
     }
 
-    @Test
-    public void verifyEventViewIsCorrectlyCalled() {
-        onView(withId(R.id.buttonEventAdd)).perform(ViewActions.click());
-
-        intended(allOf(
-                hasComponent(EventsView.class.getName()),
-                hasExtra(is(EventCreator.EXTRA_EVENT), is(instanceOf(Event.class)))
-        ));
-    }
-
+    // Those do not work on cirrus...
     private void setTimePicker(int startId, ZonedDateTime time) {
         onView(withId(startId)).perform(ViewActions.click());
         onView(withClassName(equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(time.getHour(), time.getMinute()));
@@ -111,41 +116,44 @@ public class EventCreatorTest {
     }
 
     @Test
-    public void eventSettingsCanBeChanged() {
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), EventCreator.class);
-        EventCreator.putEventExtra(intent, EVENT_BIS);
+    public void verifyEventViewIsCorrectlyCalled() {
+        onView(withId(R.id.buttonEventAdd)).perform(scrollTo(), click());
 
-        try (ActivityScenario<EventCreator> scenario = ActivityScenario.launch(intent)) {
-            onView(withId(R.id.editEventName)).perform(scrollTo(), ViewActions.clearText());
-            onView(withId(R.id.editEventLocationName)).perform(scrollTo(), ViewActions.clearText());
-
-            onView(withId(R.id.editEventName)).perform(scrollTo(), ViewActions.typeText(NAME));
-            ViewActions.closeSoftKeyboard();
-            onView(withId(R.id.editEventLocationName)).perform(scrollTo(), ViewActions.typeText(LOCATION_NAME));
-            ViewActions.closeSoftKeyboard();
-
-            /*
-             * So...
-             * For some reason, cirrus doesn't like those wonderful test
-             * functions stolen from SO. Since this is deeply tied to the current
-             * UI choice, and since I already way to much time on this, this is gonna
-             * be disabled for now. I will most likely change the UI again when I have the
-             * time, so that we can test this correctly.
-             * XOXO
-             */
-//            setTimePicker(R.id.buttonStartTime, START_TIME);
-//            setDatePicker(R.id.buttonStartDate, START_TIME);
-//            setTimePicker(R.id.buttonEndTime, END_TIME);
-//            setDatePicker(R.id.buttonEndDate, END_TIME);
-
-            onView(withId(R.id.buttonEventAdd)).perform(scrollTo(), ViewActions.click());
-
-//            intended(hasExtra(EventCreator.EXTRA_EVENT, EVENT));
-        }
+        intended(allOf(
+                hasComponent(EventsView.class.getName()),
+                hasExtra(is(EventCreator.EXTRA_EVENT), is(instanceOf(Event.class)))
+        ));
     }
 
     @Test
-    public void eventDateCanSpecified() {
+    public void geolocationTabIsOpenedWhenNoGeolocationIsSet(){
+        onView(withId(R.id.checkGeolocation)).perform(scrollTo(), click());
+
+        onView(withId(R.id.buttonSetGeolocation)).check(matches(not(isEnabled())));
+    }
+
+    @Test
+    public void geocodingCanBeUsed() {
+        onView(withId(R.id.checkGeolocation)).perform(scrollTo(), click());
+
+        onView(withId(R.id.editLocationQuery)).perform(scrollTo(), clearText(), typeText(EPFL_QUERY));
+        closeSoftKeyboard();
+        onView(withId(R.id.buttonSearchLocation)).perform(scrollTo(), click());
+        onView(withId(R.id.textSelectedLocationFull)).check(matches(withText(containsString(EPFL_CANTON))));
+    }
+
+    @Test
+    public void phoneLocationCanBeUsed() {
+        List<String> expected = List.of("lat", "lon");
+
+        onView(withId(R.id.checkGeolocation)).perform(scrollTo(), click());
+
+        onView(withId(R.id.buttonUseCurrentLocation)).perform(scrollTo(), click());
+        onView(withId(R.id.textSelectedLocationFull)).check(matches(withText(stringContainsInOrder(expected))));
+    }
+
+    @Test
+    public void eventDateCanBeSpecifiedAsInput() {
         final LocalDate date = LocalDate.of(1000, 10, 1);
 
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), EventCreator.class);
@@ -153,6 +161,28 @@ public class EventCreatorTest {
 
         try (ActivityScenario<EventCreator> scenario = ActivityScenario.launch(intent)) {
             onView(withId(R.id.buttonStartDate)).check(matches(withText(date.toString())));
+        }
+    }
+
+    @Test
+    public void eventCanBeSpecifiedAsInput() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), EventCreator.class);
+        EventCreator.putEventExtra(intent, EVENT);
+
+        try (ActivityScenario<EventCreator> scenario = ActivityScenario.launch(intent)) {
+            onView(withId(R.id.editEventName)).check(matches(withText(EVENT.getName())));
+
+            onView(withId(R.id.checkGeolocation)).check(matches(isChecked()));
+
+            onView(withId(R.id.buttonStartDate)).check(matches(withText(EVENT.getStartTime().toLocalDate().toString())));
+            onView(withId(R.id.buttonStartTime)).check(matches(withText(EVENT.getStartTime().toLocalTime().toString())));
+
+            onView(withId(R.id.buttonEndDate)).check(matches(withText(EVENT.getEndTime().toLocalDate().toString())));
+            onView(withId(R.id.buttonEndTime)).check(matches(withText(EVENT.getEndTime().toLocalTime().toString())));
+
+            onView(withId(R.id.buttonGotoGeolocation)).perform(scrollTo(), click());
+
+            onView(withId(R.id.textSelectedLocationFull)).check(matches(withText(EVENT.getLocation().get().toString())));
         }
     }
 }
