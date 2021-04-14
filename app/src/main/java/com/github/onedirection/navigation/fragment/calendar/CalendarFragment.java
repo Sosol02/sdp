4b6a195124f88917281dacc72962d174f7a719dc
Paulo <paul.oliver4@protonmail.com>
2021-04-14 17:46:07 +0200

    Implement an extremely cursed version of the calendar, view it at your own risk

--- app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CalendarFragment.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CalendarFragment.java
@@ -28,7 +28,7 @@ public class CalendarFragment extends Fragment {
 
     private final String EXTRA_MESSAGE_DATE = "DATE";
     private CalendarViewModel mViewModel;
-    private CalenderEvent calendarView;
+    private CustomCalendarView calendarView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -41,37 +41,37 @@ public class CalendarFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(CalendarViewModel.class);
-        calendarView.initCalderItemClickCallback(dayContainerModel -> {
-            DaySelectPopupFragment popup = new DaySelectPopupFragment(this, dayContainerModel.getDay(), dayContainerModel.getMonthNumber() + 1, dayContainerModel.getYear());
-            popup.show(getChildFragmentManager(), getResources().getString(R.string.day_select_dialog_tag));
-        });
-
-        EventQueries.getEventsByMonth(Database.getDefaultInstance(), ZonedDateTime.now()).thenAccept(events -> {
-           for(com.github.onedirection.events.Event event : events){
-               addEventToCalendar(event);
-               // TODO: Check with Paul why nothing is display
-           }
-        });
+//        mViewModel = new ViewModelProvider(this).get(CalendarViewModel.class);
+//        calendarView.initCalderItemClickCallback(dayContainerModel -> {
+//            DaySelectPopupFragment popup = new DaySelectPopupFragment(this, dayContainerModel.getDay(), dayContainerModel.getMonthNumber() + 1, dayContainerModel.getYear());
+//            popup.show(getChildFragmentManager(), getResources().getString(R.string.day_select_dialog_tag));
+//        });
+//
+//        EventQueries.getEventsByMonth(Database.getDefaultInstance(), ZonedDateTime.now()).thenAccept(events -> {
+//           for(com.github.onedirection.events.Event event : events){
+//               addEventToCalendar(event);
+//               // TODO: Check with Paul why nothing is display
+//           }
+//        });
     }
 
     public void addEventToCalendar(com.github.onedirection.events.Event event) {
-        Objects.requireNonNull(event, "Tried to add null Event");
-        Event calendarEvent = new Event(event.getStartTime().toInstant().toEpochMilli(), "AAAAAH"/*event.getName()*/);
-        calendarView.addEvent(calendarEvent);
+//        Objects.requireNonNull(event, "Tried to add null Event");
+//        Event calendarEvent = new Event(event.getStartTime().toInstant().toEpochMilli(), "AAAAAH"/*event.getName()*/);
+//        calendarView.addEvent(calendarEvent);
 
     }
 
     public void removeEventFromCalendar(Event calendarEvent) {
-        Objects.requireNonNull(calendarEvent, "Tried to remove null calendarEvent");
-        calendarView.removeEvent(calendarEvent);
+//        Objects.requireNonNull(calendarEvent, "Tried to remove null calendarEvent");
+//        calendarView.removeEvent(calendarEvent);
     }
 
     public void showEventCreationScreen(int day, int month, int year) {
-        Intent intent = new Intent(getActivity(), EventCreator.class);
-        Log.d("AAAAAAAAAAAAAAAAAAAAAAAAAAH", "month is AAAAAAAAAAAAA" + month );
-        EventCreator.putDateExtra(intent, LocalDate.of(year, month, day));
-        new Event(LocalDate.of(year, month, day).toEpochDay(), "OOOH");
-        startActivity(intent);
+//        Intent intent = new Intent(getActivity(), EventCreator.class);
+//        Log.d("AAAAAAAAAAAAAAAAAAAAAAAAAAH", "month is AAAAAAAAAAAAA" + month );
+//        EventCreator.putDateExtra(intent, LocalDate.of(year, month, day));
+//        new Event(LocalDate.of(year, month, day).toEpochDay(), "OOOH");
+//        startActivity(intent);
     }
 }
--- app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CustomCalendarView.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CustomCalendarView.java
@@ -2,12 +2,16 @@ package com.github.onedirection.navigation.fragment.calendar;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import com.github.onedirection.R;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -22,6 +26,7 @@ public class CustomCalendarView extends LinearLayout {
     TextView CurrentDate;
     GridView gridView;
 
+
     Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
     Context context;
     List<Date> dates = new ArrayList<>();
@@ -35,5 +40,44 @@ public class CustomCalendarView extends LinearLayout {
     public CustomCalendarView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
+        InitializeLayout();
+        SetUpCalendar();
+
+        PreviousButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, -1);
+                SetUpCalendar();
+            }
+        });
+
+        NextButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, 1);
+                SetUpCalendar();
+            }
+        });
+    }
+
+    public CustomCalendarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void InitializeLayout(){
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_calendar_view, this);
+        NextButton = view.findViewById(R.id.nextBtn);
+        PreviousButton = view.findViewById(R.id.previousBtn);
+        CurrentDate = view.findViewById(R.id.currentDate);
+        gridView = view.findViewById(R.id.gridView);
+    }
+
+    private void SetUpCalendar(){
+        Date currentDate = calendar.getTime();
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int monthOfYear = calendar.get(Calendar.MONTH) + 1;
+        int year = calendar.get(Calendar.YEAR);
+        CurrentDate.setText(dayOfMonth + " / " + monthOfYear + " / " + year);
     }
 }
