49218d982558a71c251e951fc05f34e899281543
Noe De Santo <61206225+Ef55@users.noreply.github.com>
2021-04-15 23:00:30 +0200

    Link database with EventCreator (and Calendar) (#105)
    * Link database with EventCreator (and Calendar)
    
    The calendar currently doesn't display anything (for some reason).
    
    * Fix CalendarFragmentTest
    
    * Fix EventCreatorTest
    
    * Improve coverage for EventCreator and EventsView

--- app/src/androidTest/java/com/github/onedirection/EventCreatorTest.java
+++ app/src/androidTest/java/com/github/onedirection/EventCreatorTest.java
@@ -135,16 +135,6 @@ public class EventCreatorTest {
     }
 
     @Test
-    public void verifyEventViewIsCorrectlyCalled() {
-        onView(withId(R.id.buttonEventAdd)).perform(scrollTo(), click());
-
-        intended(allOf(
-                hasComponent(EventsView.class.getName()),
-                hasExtra(is(EventCreator.EXTRA_EVENT), is(instanceOf(Event.class)))
-        ));
-    }
-
-    @Test
     public void geolocationTabIsOpenedWhenNoGeolocationIsSet(){
         onView(withId(R.id.checkGeolocation)).perform(scrollTo(), click());
 
@@ -224,6 +214,10 @@ public class EventCreatorTest {
             onView(withId(R.id.buttonGotoGeolocation)).perform(scrollTo(), click());
 
             onView(withId(R.id.textSelectedLocationFull)).check(matches(withText(EVENT.getLocation().get().toString())));
+
+            onView(withId(R.id.buttonCancelGeolocation)).perform(scrollTo(), click());
+
+            onView(withId(R.id.buttonEventAdd)).perform(scrollTo(), click());
         }
     }
 
--- app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CalendarFragment.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/calendar/CalendarFragment.java
@@ -11,12 +11,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.onedirection.EventQueries;
+import com.github.onedirection.database.Database;
 import com.github.onedirection.events.EventCreator;
 import com.github.onedirection.R;
 import com.skyhope.eventcalenderlibrary.CalenderEvent;
 import com.skyhope.eventcalenderlibrary.model.Event;
 
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 public class CalendarFragment extends Fragment {
@@ -41,16 +44,23 @@ public class CalendarFragment extends Fragment {
             DaySelectPopupFragment popup = new DaySelectPopupFragment(this, dayContainerModel.getDay(), dayContainerModel.getMonthNumber(), dayContainerModel.getYear());
             popup.show(getChildFragmentManager(), getResources().getString(R.string.day_select_dialog_tag));
         });
+
+        EventQueries.getEventsByMonth(Database.getDefaultInstance(), ZonedDateTime.now()).thenAccept(events -> {
+           for(com.github.onedirection.events.Event event : events){
+               addEventToCalendar(event);
+               // TODO: Check with Paul why nothing is display
+           }
+        });
     }
 
-    public void addEventToCalendar(com.github.onedirection.events.Event event, long timeInMillis) {
-        Objects.requireNonNull(event, "tried to add null Event");
-        Event calendarEvent = new Event(timeInMillis, event.getName());
+    public void addEventToCalendar(com.github.onedirection.events.Event event) {
+        Objects.requireNonNull(event, "Tried to add null Event");
+        Event calendarEvent = new Event(event.getStartTime().toInstant().toEpochMilli(), event.getName());
         calendarView.addEvent(calendarEvent);
     }
 
     public void removeEventFromCalendar(Event calendarEvent) {
-        Objects.requireNonNull(calendarEvent, "tried to remove null calendarEvent");
+        Objects.requireNonNull(calendarEvent, "Tried to remove null calendarEvent");
         calendarView.removeEvent(calendarEvent);
     }
 
