c6ee39d9066c7296a87f5bcbdc5363a17dde56b5
Noe De Santo <noe.desanto@epfl.ch>
2021-04-26 17:18:17 +0200

    Change GeocodingCache to use utils.Pair instead of custom internal class.

--- app/src/main/java/com/github/onedirection/geolocation/geocoding/GeocodingCache.java
+++ app/src/main/java/com/github/onedirection/geolocation/geocoding/GeocodingCache.java
@@ -3,6 +3,7 @@ package com.github.onedirection.geolocation.geocoding;
 import com.github.onedirection.geolocation.Coordinates;
 import com.github.onedirection.geolocation.NamedCoordinates;
 import com.github.onedirection.utils.Cache;
+import com.github.onedirection.utils.Pair;
 
 import java.util.List;
 import java.util.Objects;
@@ -19,36 +20,12 @@ public final class GeocodingCache implements GeocodingService {
      * so this shouldn't be a real issue.
      */
 
-    private static class Key{
-        public final String str;
-        public final int i;
-
-        public Key(String str, int i){
-            this.str = str;
-            this.i = i;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Key stringInt = (Key) o;
-            return i == stringInt.i &&
-                    Objects.equals(str, stringInt.str);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(str, i);
-        }
-    }
-
-    private final Cache<Key, CompletableFuture<List<NamedCoordinates>>> nameCache;
+    private final Cache<Pair<String, Integer>, CompletableFuture<List<NamedCoordinates>>> nameCache;
     private final Cache<Coordinates, CompletableFuture<NamedCoordinates>> coordinatesCache;
 
     public GeocodingCache(GeocodingService underlying){
         nameCache = new Cache<>(
-                key -> underlying.getNamedCoordinates(key.str, key.i)
+                key -> underlying.getNamedCoordinates(key.first, key.second)
         );
         coordinatesCache = new Cache<>(
                 underlying::getBestNamedCoordinates
@@ -57,7 +34,7 @@ public final class GeocodingCache implements GeocodingService {
 
     @Override
     public CompletableFuture<List<NamedCoordinates>> getNamedCoordinates(String locationName, int count) {
-        return nameCache.get(new Key(locationName, count));
+        return nameCache.get(new Pair<>(locationName, count));
     }
 
     @Override
