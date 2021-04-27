4766727f02a08a1da68225776dae97a6bdf3f008
Paulo <paul.oliver4@protonmail.com>
2021-04-27 10:12:00 +0200

    Add LoadingDialog.java and loading_dialog.xml

---
+++ app/src/main/java/com/github/onedirection/utils/LoadingDialog.java
@@ -0,0 +1,31 @@
+package com.github.onedirection.utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.github.onedirection.R;
+
+public class LoadingDialog {
+    private final Activity activity;
+    private AlertDialog dialog;
+
+    public LoadingDialog(Activity hostActivity) {
+        activity = hostActivity;
+    }
+
+    public void startLoadingAnimation() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        LayoutInflater inflater = activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.loading_dialog, null));
+        builder.setCancelable(false);
+
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    public void dismissDialog() {
+        dialog.dismiss();
+    }
+}
