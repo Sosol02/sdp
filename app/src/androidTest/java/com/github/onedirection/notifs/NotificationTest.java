64e79683560b7cf86c6bd27adf41daa87a32c2dd
Solène Husseini <solene.husseini@gmail.com>
2021-05-03 18:07:29 +0200

    Save progress before major overhaul of the system

--- app/src/androidTest/java/com/github/onedirection/notifs/NotificationTest.java
+++ app/src/androidTest/java/com/github/onedirection/notifs/NotificationTest.java
@@ -7,7 +7,9 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.github.onedirection.R;
+import com.github.onedirection.database.ConcreteDatabase;
 import com.github.onedirection.database.Database;
+import com.github.onedirection.database.store.EventStorer;
 import com.github.onedirection.events.Event;
 import com.github.onedirection.navigation.NavigationActivity;
 import com.github.onedirection.testhelpers.WaitAction;
@@ -21,11 +23,13 @@ import org.junit.runner.RunWith;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class NotificationTest {
@@ -36,8 +40,13 @@ public class NotificationTest {
     public ActivityScenarioRule<NavigationActivity> testRule = new ActivityScenarioRule<>(NavigationActivity.class);
 
     @Before
-    public void init() {
-
+    public void deleteAllEvents() throws ExecutionException, InterruptedException {
+        ConcreteDatabase db = ConcreteDatabase.getDatabase();
+        List<Event> events = db.retrieveAll(EventStorer.getInstance()).get();
+        for(Event e : events) {
+            Id id = db.remove(e.getId(), EventStorer.getInstance()).get();
+            assertEquals(e.getId(), id);
+        }
     }
 
     @Test
@@ -46,17 +55,15 @@ public class NotificationTest {
         //final ArrayList<Event> events = new ArrayList<>();
         for (int i = 0; i < 10; ++i) {
             db.store(new Event(Id.generateRandom(), "Event" + i, "Place" + i, Optional.empty(),
-                    ZonedDateTime.now().plusSeconds(10 * i),
-                    ZonedDateTime.now().plusSeconds(10 * i + 1),
+                    ZonedDateTime.now().plusSeconds(5 * i),
+                    ZonedDateTime.now().plusSeconds(5 * i + 1),
                     Optional.empty())).get();
         }
-
-
 
         Notifications notifs = Notifications.getInstance(ctx);
         notifs.scheduleClosestEvent(ctx);
 
-        onView(withId(R.id.textExampleHome)).perform(new WaitAction(60000));
+        onView(withId(R.id.textExampleHome)).perform(new WaitAction(600000));
 
     }
 
