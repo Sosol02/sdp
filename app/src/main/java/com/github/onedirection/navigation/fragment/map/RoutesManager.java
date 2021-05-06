5bc2c893dda63286bdf1dfa24e0dc7be694b9abd
Nicolas Wagner <nicolas.wagner@epfl.ch>
2021-05-06 14:12:04 +0200

    split routesManager into two class, one that fetch that can fecth the route the other can display it, so we can use it for navigation easier

--- app/src/main/java/com/github/onedirection/navigation/fragment/map/RoutesManager.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/map/RoutesManager.java
@@ -1,24 +1,16 @@
 package com.github.onedirection.navigation.fragment.map;
 
 import android.content.Context;
-import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import com.github.onedirection.BuildConfig;
-import com.github.onedirection.utils.EspressoIdlingResource;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.plugins.annotation.Line;
-import com.mapbox.mapboxsdk.plugins.annotation.LineManager;
-import com.mapbox.mapboxsdk.plugins.annotation.LineOptions;
 import com.mapquest.navigation.dataclient.RouteService;
 import com.mapquest.navigation.dataclient.listener.RoutesResponseListener;
 import com.mapquest.navigation.model.Route;
-import com.mapquest.navigation.model.RouteLeg;
 import com.mapquest.navigation.model.RouteOptionType;
 import com.mapquest.navigation.model.RouteOptions;
 import com.mapquest.navigation.model.SystemOfMeasurement;
@@ -26,37 +18,36 @@ import com.mapquest.navigation.model.location.Coordinate;
 import com.mapquest.navigation.model.location.Destination;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import io.opencensus.internal.DefaultVisibilityForTesting;
+import java.util.Objects;
 
 public class RoutesManager {
 
-    private LineManager lineManager;
     private RouteService routeService;
     private List<Route> routes;
-    private List<Line> lines;
 
-    private final String LINE_COLOR = "Blue";
-    private final float LINE_WIDTH = 2f;
-
-    public RoutesManager(Context context, MapView mapView, MapboxMap mapboxMap, Style style) {
-        this.lineManager = new LineManager(mapView, mapboxMap, style);
+    public RoutesManager(Context context) {
+        Objects.requireNonNull(context);
         this.routeService = new RouteService.Builder().build(context,
                 BuildConfig.API_KEY);
     }
 
     public void findRoute(LatLng start, LatLng finish) {
-        findRoute(start, finish, () -> {});
+        findRoute(start, finish, new RoutesResponseListener() {
+            @Override
+            public void onRoutesRetrieved(@NonNull List<Route> list) {}
+
+            @Override
+            public void onRequestFailed(@Nullable Integer integer, @Nullable IOException e) {}
+
+            @Override
+            public void onRequestMade() {}
+        });
     }
 
-    @VisibleForTesting
-    public void findRoute(LatLng start, LatLng finish, Runnable runnableOnSuccess) {
-        clearRoute();
-        clearLines();
+    public void findRoute(LatLng start, LatLng finish, RoutesResponseListener routesResponseListener) {
+        clearRoutes();
         Coordinate from = new Coordinate(start.getLatitude(), start.getLongitude());
         Coordinate tmp = new Coordinate(finish.getLatitude(), finish.getLongitude());
         List<Destination> to = Arrays.asList(new Destination(tmp, null));
@@ -78,67 +69,23 @@ public class RoutesManager {
             public void onRoutesRetrieved(List<Route> routes1) {
                 if (routes1.size() > 0) {
                     routes = routes1;
-                    displayRoute(routes.get(0));
                 }
-                runnableOnSuccess.run();
+                routesResponseListener.onRoutesRetrieved(routes1);
             }
 
             @Override
-            public void onRequestFailed(@Nullable Integer httpStatusCode, @Nullable IOException exception) {}
+            public void onRequestFailed(@Nullable Integer httpStatusCode, @Nullable IOException exception) {
+                routesResponseListener.onRequestFailed(httpStatusCode, exception);
+            }
 
             @Override
-            public void onRequestMade() {}
+            public void onRequestMade() {
+                routesResponseListener.onRequestMade();
+            }
         });
     }
 
-    private void clearRoute() {
+    public void clearRoutes() {
         routes = null;
     }
-
-    private void clearLines() {
-        lines = null;
-        lineManager.deleteAll();
-    }
-
-    public void clearRoutesManager() {
-        clearLines();
-        clearRoute();
-    }
-
-    private void displayRoute(Route selectedRoute) {
-        clearLines();
-        if (routes == null) {
-            throw new IllegalStateException("Routes should't be empty when trying to display a route");
-        }
-
-        if (!routes.contains(selectedRoute)) {
-            throw new IllegalStateException("Selected route should be in routes");
-        }
-
-        lines = createLinesRoute(selectedRoute.getLegs());
-    }
-
-    private List<Line> createLinesRoute(List<RouteLeg> routeLegs) {
-        if (routeLegs == null) {
-            throw new IllegalArgumentException("RoutesLegs should not be null");
-        }
-        List<Line> linesRoute = new ArrayList<>();
-        for (RouteLeg routeLeg: routes.get(0).getLegs()) {
-            List<Coordinate> coordinates = routeLeg.getShape().getCoordinates();
-            List<LatLng> latLngs = coordinates.stream().map(coordinate ->
-                    new LatLng(coordinate.getLatitude(), coordinate.getLongitude()))
-                    .collect(Collectors.toList());
-            linesRoute.add(createLineRoute(latLngs));
-        }
-        return linesRoute;
-    }
-
-    private Line createLineRoute(List<LatLng> latLngs) {
-        LineOptions lineOptions = new LineOptions()
-                .withLatLngs(latLngs)
-                .withLineWidth(LINE_WIDTH)
-                .withLineColor(LINE_COLOR);
-        return lineManager.create(lineOptions);
-    }
-
 }
