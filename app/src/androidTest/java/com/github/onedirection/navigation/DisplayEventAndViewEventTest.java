fd28967a6f913232d1102de4ee17604bde6b8769
hugo <hugo.lanfranchi@gmail.com>
2021-05-07 00:15:53 +0200

    fix import in test

--- app/src/androidTest/java/com/github/onedirection/navigation/DisplayEventAndViewEventTest.java
+++ app/src/androidTest/java/com/github/onedirection/navigation/DisplayEventAndViewEventTest.java
@@ -16,7 +16,7 @@ import androidx.test.runner.AndroidJUnit4;
 
 import com.github.onedirection.R;
 import com.github.onedirection.events.Event;
-import com.github.onedirection.events.EventCreator;
+import com.github.onedirection.events.ui.EventCreator;
 import com.github.onedirection.eventviewer.DisplayEvent;
 import com.github.onedirection.geolocation.NamedCoordinates;
 import com.github.onedirection.utils.Id;
