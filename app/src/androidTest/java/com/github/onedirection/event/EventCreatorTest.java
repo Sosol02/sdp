1991d0cb0478f828b2241e2b561c488fe9c10273
Remi <54138269+Flechman@users.noreply.github.com>
2021-05-13 12:16:53 +0200

    First version of the general theme of the UI + UI for the event creation (#202)
    * event creation ui done + app main theme done
    
    * remove lines to make previous tests pass
    
    * correct event creator view for tests to pass
    
    * change px to do as requested
    
    * attempt to resolve the cirrus scroll errors + make calendar event popup background transparent as requested
    
    * unsignificant change, just to make cirrus run again the tests
    
    * remove menu_event_viewer icon & correct some lint warnings
    
    * recover precious state for the tests to pass
    
    * restart tests

--- app/src/androidTest/java/com/github/onedirection/event/EventCreatorTest.java
+++ app/src/androidTest/java/com/github/onedirection/event/EventCreatorTest.java
@@ -129,11 +129,11 @@ public class EventCreatorTest {
     }
 
     public void testIsMainFragment() {
-        onView(ViewMatchers.withId(R.id.textEventCreatorTitle)).check(matches(withText(containsString("Create"))));
+        onView(ViewMatchers.withId(R.id.textEventCreatorTitle)).check(matches(withText(containsString("CREATE"))));
     }
 
     public void testIsGeolocationFragment() {
-        onView(withId(R.id.textEventCreatorTitle)).check(matches(withText(containsString("location"))));
+        onView(withId(R.id.textEventCreatorTitle)).check(matches(withText(containsString("LOCATION"))));
     }
 
     ////////////////////////////////////////////////////////////
