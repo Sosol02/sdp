13bd18ab33f5912b8e16ac7cfa3111231bdc2971
hugo <hugo.lanfranchi@gmail.com>
2021-05-14 00:10:04 +0200

    modified event apparence

--- app/src/main/java/com/github/onedirection/navigation/fragment/home/EventViewerAdapter.java
+++ app/src/main/java/com/github/onedirection/navigation/fragment/home/EventViewerAdapter.java
@@ -56,7 +56,11 @@ public class EventViewerAdapter extends RecyclerView.Adapter<EventViewerAdapter.
 
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss");
             this.name.setText(events[position].getName());
-            this.location.setText(events[position].getLocationName());
+            if(events[position].getLocationName() != ""){
+                this.location.setText(events[position].getLocationName());
+            }else{
+                this.location.setText("No location precised ");
+            }
             this.startTime.setText(events[position].getStartTime().format(formatter));
             this.endTime.setText(events[position].getEndTime().format(formatter));
 
