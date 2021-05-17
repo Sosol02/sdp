ced0a7f3cf873b8586dbc7ccb094b3fa37e014c8
Delacourt <remi.delacourt@gmail.com>
2021-05-17 16:19:31 +0200

    gc events conversion start

--- app/src/main/java/com/github/onedirection/interoperability/gcalendar/GoogleCalendar.java
+++ app/src/main/java/com/github/onedirection/interoperability/gcalendar/GoogleCalendar.java
@@ -10,11 +10,15 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.CalendarList;
 import com.google.api.services.calendar.model.CalendarListEntry;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -28,6 +32,7 @@ public final class GoogleCalendar {
     // Don't ask my the hows-and-whys of this string, all I know is that it doesn't work otherwise
     private static final String OAUTH_SCOPE = "oauth2:profile email";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String RFC3339_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXX";
     static final String LOGCAT_TAG = "GCalendar";
 
     private GoogleCalendar() {
@@ -43,7 +48,25 @@ public final class GoogleCalendar {
         Objects.requireNonNull(event);
         Event gcEvent = new Event()
                 .setSummary(event.getName());
+        if(event.getLocation().isPresent()) {
+            gcEvent.setLocation();
+        }
 
+        DateTime startDateTime = new DateTime(event.getStartTime().format(DateTimeFormatter.ofPattern(RFC3339_FORMAT)));
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDateTime)
+                .setTimeZone(ZoneId.SHORT_IDS.get(event.getStartTime().getZone().getId()));
+        gcEvent.setStart(start);
+
+        DateTime endDateTime = new DateTime(event.getEndTime().format(DateTimeFormatter.ofPattern(RFC3339_FORMAT)));
+        EventDateTime end = new EventDateTime()
+                .setDateTime(endDateTime)
+                .setTimeZone(ZoneId.SHORT_IDS.get(event.getEndTime().getZone().getId()));
+        gcEvent.setEnd(end);
+
+        if(event.isRecurrent()) {
+
+        }
 
 
         return null;
