af0bc4bfb5eac0fdcc12fca4c56eef36fab2b74a
Noe De Santo <61206225+Ef55@users.noreply.github.com>
2021-05-26 15:42:44 +0200

    Setup GCalendar import (#277)
    * Setup GCalendar import
    
    * Add test for GCalendar import fragment
    
    * Add class comments to GCalendar interop
    
    * Add package info to GCalendar interop
    
    * Fix GCalendar importing some events it shouldn't

--- app/src/main/java/com/github/onedirection/interoperability/gcalendar/ImportFragment.java
+++ app/src/main/java/com/github/onedirection/interoperability/gcalendar/ImportFragment.java
@@ -1,5 +1,6 @@
 package com.github.onedirection.interoperability.gcalendar;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,23 +8,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.fragment.app.Fragment;
 
 import com.github.onedirection.R;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.tasks.Task;
+
+import java.util.Objects;
 
 import static com.github.onedirection.interoperability.gcalendar.GoogleCalendar.LOGCAT_TAG;
+import static com.github.onedirection.interoperability.gcalendar.Login.createLogger;
+import static com.github.onedirection.interoperability.gcalendar.Login.logIn;
+import static com.github.onedirection.interoperability.gcalendar.Login.logOut;
 
+/**
+ * Fragment which allows to import the calendar into Google Calendar.
+ */
 public class ImportFragment extends Fragment {
+
+    private ActivityResultLauncher<Intent> loginGoogle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        this.loginGoogle = createLogger(this, this::importEvents);
     }
 
     @Override
@@ -32,11 +41,23 @@ public class ImportFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.gcalendar_fragment_import, container, false);
 
-        view.findViewById(R.id.buttonGCalendarImport).setOnClickListener(this::doSomething);
+        view.findViewById(R.id.buttonGCalendarImport).setOnClickListener(this::googleLogin);
 
         return view;
     }
 
-    private void doSomething(View ignored) {
+
+    private void googleLogin(View ignored) {
+        logIn(requireContext(), loginGoogle);
+    }
+
+    private void importEvents(GoogleSignInAccount account) {
+        Account a = Objects.requireNonNull(Objects.requireNonNull(account).getAccount());
+
+        Log.d(LOGCAT_TAG, "Events will be imported from: " + a.name);
+        GoogleCalendar.importEvent(
+                requireContext(),
+                a
+        ).whenComplete((ignored1, ignored2) -> logOut(requireContext()));
     }
 }
---
+++ app/src/main/java/com/github/onedirection/interoperability/gcalendar/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provide utilities and UIs to interact with Google Calendar.
+ */
+package com.github.onedirection.interoperability.gcalendar;
