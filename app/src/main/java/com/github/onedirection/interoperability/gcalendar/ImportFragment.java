8b82241c79ed649be3fcaa455033dd843889111c
Remi <remi.delacourt@gmail.com>
2021-05-26 16:10:58 +0200

    add missing import

--- app/src/main/java/com/github/onedirection/interoperability/gcalendar/ImportFragment.java
+++ app/src/main/java/com/github/onedirection/interoperability/gcalendar/ImportFragment.java
@@ -3,6 +3,7 @@ package com.github.onedirection.interoperability.gcalendar;
 import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
